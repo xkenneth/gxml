272f9685dfe34f7a285716d9dbf12604dc4bf6f1
Kenneth Miller <xkenneth@gmail.com>
2008-10-26 23:06:06 -0500

    Adding setup and __init__

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+    name = "gxml",
+    version = "0.1",
+    packages = find_packages(),
+
+    author = "Kenneth Miller",
+    author_email = "xkenneth@gmail.com",
+    description = "A good XML abstraction.",
+    keywords = "python xml elementtree lxml minidom",
+    url = "http://www.xkenneth.com/gxml",
+)
